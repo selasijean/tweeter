83e4e2950f002c73e58e351f4e3e93cf725c6f79
Jean Adedze <selasijean@fb.com>
2016-06-27 13:35:57 -0700

    Initial Commit

---
+++ Tweetr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweetr
+//
+//  Created by Jean Adedze on 6/27/16.
+//  Copyright © 2016 Jean Adedze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
